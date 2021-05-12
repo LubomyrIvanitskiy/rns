5cd224045a87ea681915267d0f323a45d032395f
liubomyr <lubomyr.ivanitskiy@gmail.com>
2021-05-12 14:40:02 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rns',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/LubomyrIvanitskiy/rns',
+    license='MIT',
+    author='liubomyr.ivanitskyi',
+    author_email='lubomyr.ivanitskiy@gmail.com',
+    description='Residual Number System Conversion'
+)
