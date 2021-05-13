6958da87c556b843988d84513aec97e7fe80fc03
liubomyr <lubomyr.ivanitskiy@gmail.com>
2021-05-13 16:13:44 +0300

    Performance boost. Simplify solve method Don't use numpy lcm Use product instead of LCM when moduli are co-prime

--- rns.py
+++ rns.py
@@ -1,14 +1,13 @@
-import numpy as np
-from itertools import accumulate
+from math import gcd
 
 
-def _printer(i, a, m, k, m_a, k_a, z, x):
+def _printer(i, a, m, k, x):
     print(f"{a}x%{m}={k}")
     print(f"x_{i}={x}")
     print()
 
 
-def solve(a, m, k, i=0, prnt=False, allow_zero=False):
+def solve(a, m, k):
     """
     Solve simple linear conguruence equation
     Find the least x such that ax=k (mod m)
@@ -16,54 +15,24 @@ def solve(a, m, k, i=0, prnt=False, allow_zero=False):
     if allow_zero=False 2x%3=0, x=3
     """
 
-    if m > k:
-        if k == a == 0:
-            return k
-        elif k % a == 0:
-            return k // a
-
-    assert a > 0
-
-    k_a = k % a
-    m_a = m % a
-    m_a = m_a if m_a > 0 else a
-    z = a - k_a
-    z = z if z > 0 else a
+    if a == 0:
+        if k == 0:
+            return 0
+        else:
+            raise ValueError(f"{a}x%{m}={k} - No SOLUTIONS")
 
     if a == 1:
-        x = k % m
-        if not allow_zero:
-            x = x if x > 0 else k
-        if prnt:
-            print('case 1')
-            _printer(i, a, m, k, m_a, k_a, z, x)
-        return x
+        return k % m
 
-    if m % a == 0:
-        if k % m % a == 0:
-            x = k % m // a
-            if not allow_zero:
-                x = x if x > 0 else k // a
-            if prnt:
-                print('case 2')
-                _printer(i, a, m, k, m_a, k_a, z, x)
-            return x
-        else:
-            print(f"{a}x%{m}={k} - No SOLUTIONS")
-            raise ValueError('No solutions')
+    if k % a == 0:
+        return k // a
 
     new_x = solve(
-        a=m_a,
+        a=m % a,
         m=a,
-        k=z,
-        i=i + 1,
-        prnt=prnt,
-        allow_zero=allow_zero
+        k=(a - k) % a
     )
     x = (k + new_x * m) // a
-    if prnt:
-        print('case 3')
-        _printer(i, a, m, k, m_a, k_a, z, x)
     return x
 
 
@@ -76,52 +45,56 @@ def get_dynamic_range(P):
     >>> get_dynamic_range([4,3,2])
     12
     """
-    return np.lcm.reduce(P)
+    r = 1
+    for p in P:
+        r = getLCM(r, p)
+    return r
 
 
-def encode(n, P):
+def encode(n, P, check_range=True):
     """
     Encode an integer decimal number n into corresponding RNS array
     >>> encode(n=14221, P=[21,19,18,11,10,8,7,5,4])
     [4, 9, 1, 9, 1, 5, 4, 1, 1]
     """
-    if n >= get_dynamic_range(P):
+    if check_range and n >= get_dynamic_range(P):
         raise ValueError("You cannot encode such a big number with given moduli. Try to increase moduli dynamic range")
-    code = [n % P[-i] for i in range(1, len(P) + 1)][::-1]
-    return code
+    return [(n % q) for q in P]
 
 
-def decode(code, P):
+def getLCM(a, b):
+    """
+    :param a: int
+    :param b: int
+    :return: least common multiple
+    """
+    return a * b // gcd(a, b)
+
+
+def decode(code, P, is_coprime=False):
     """
     Decode an RNS representation array into decimal number
+    :param is_coprime: set it to True if you know the moduli is co-prime. It will speedup a computaion
     :param P: list of moduli in order from bigger to smaller [pn, .., p2, p1, p0]
 
     >>> decode(code=[5, 3, 1], P=[7,6,5])
     201
     """
-    lcms = np.fromiter(accumulate(P[::-1], np.lcm), int)[::-1]
 
-    n = code[-1] % P[-1]
+    k = code[-1] % P[-1]
+    lcm = 1
 
     for i in range(1, len(P)):
+        c = code[-i - 1]
+        a = P[-i - 1]
 
-        bottom_p = lcms[-i]
-
-        per_diff = bottom_p % P[-i - 1]  # rev
-
-        current_next = n % P[-i - 1]
-        wanted_next = code[-i - 1] % P[-i - 1]
-        if wanted_next < current_next:
-            wanted_next = wanted_next + P[-i - 1]
-
-        distance = wanted_next - current_next
-
-        distance = distance % P[-i - 1]
+        distance = (c - k) % a
         if distance > 0:
-            bottomp_scroll_count = solve(a=per_diff, m=P[-i - 1], k=distance, allow_zero=True)
-            n = n + bottomp_scroll_count * bottom_p
+            lcm = lcm * P[-i] if is_coprime else getLCM(lcm, P[-i])
+            x = solve(a=lcm % a, m=a, k=distance)
+            k = k + x * lcm
 
-    return n
+    return k
 
 
 def list_encodings(P):
